98cff96eda8b94b2b9f412e524a06833f659aab5
adessoliers <70506634+adessoliers@users.noreply.github.com>
2020-10-21 18:20:53 -0700

    Final submission of HW

--- API-Challenge/WeatherPy/API_KEYS.py
+++ API-Challenge/WeatherPy/API_KEYS.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "5be78fade1727ace968b5ab363d997bd"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyD1x5E4ChAHo0Xc9RRBhNJGN32YoOWtDxM"
