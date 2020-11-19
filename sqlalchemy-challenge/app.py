8d8da7fb4d6d273f49ce97b641d97aa85d217999
adessoliers <alexdessoliers@yahoo.com>
2020-11-18 17:44:47 -0800

    Committing work

--- sqlalchemy-challenge/app.py
+++ sqlalchemy-challenge/app.py
@@ -9,4 +9,91 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import func
 import datetime as dt
 from dateutil.relativedelta import relativedelta
-import pandas as 
+import pandas as pd
+
+#Create engine
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+#Reflect database into model
+Base = automap_base()
+
+#Reflect tables
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#Create session from python to DB
+session = Session(engine)
+
+#Create app
+app = Flask(__name__)
+
+#home page
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br>"
+        f"/api/v1.0/stations<br>"
+        f"/api/v1.0/tobs<br>"
+        f"/api/v1.0/ + start date<br>"
+        f"/api/v1.0/ + start date/ + end date"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    print("Server received request for 'precipitation' page...")
+    last_date = session.query(func.max(Measurement.date)).all()
+    last_date = last_date[0][0]
+    last_date = dt.datetime.strptime(last_date, '%Y-%m-%d').date()
+    year_before_date = ((last_date - relativedelta(years = 1)).strftime('%Y-%m-%d'))
+    query = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_before_date).order_by(Measurement.date.desc()).all()
+    data = {date: prcp for date, prcp in query}
+    return jsonify(data)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    print("Server received request for 'stations' page...")
+    query = session.query(Station.station).all()
+    stations = list(np.ravel(query))
+    return jsonify(stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    print("Server received request for 'tobs' page...")
+    last_date = session.query(func.max(Measurement.date)).all()
+    last_date = last_date[0][0]
+    last_date = dt.datetime.strptime(last_date, '%Y-%m-%d').date()
+    year_before_date = ((last_date - relativedelta(years = 1)).strftime('%Y-%m-%d'))
+    query = session.query(Measurement.tobs).filter(Measurement.date >= year_before_date).order_by(Measurement.date.desc()).all()
+    data = list(np.ravel(query))
+    return jsonify(data)
+
+@app.route("/api/v1.0/<start>")
+def startDate(start):
+    print("Server received request for 'start' page...")
+    query = session.query(Measurement.tobs).filter(Measurement.date >= start).all()
+    df = pd.DataFrame(query)
+    tmin = df.min()
+    tavg = df.mean()
+    tmax = df.max()
+    data = [tmin, tavg, tmax]
+    data = list(np.ravel(data))
+    return jsonify(data)
+
+@app.route("/api/v1.0/<start>/<end>")
+def startANDendDate(start, end):
+    print("Server received request for 'start/end' page...")
+    query = session.query(Measurement.tobs).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    df = pd.DataFrame(query) 
+    tmin = df.min()
+    tavg = df.mean()
+    tmax = df.max()
+    data = [tmin, tavg, tmax]
+    data = list(np.ravel(data))
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
