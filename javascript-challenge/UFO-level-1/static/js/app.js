99f77ba5fc2e2328ab978f2fed3bed832e01cf5e
adessoliers <alexdessoliers@yahoo.com>
2020-12-18 03:18:02 -0800

    coding app.js

--- javascript-challenge/UFO-level-1/static/js/app.js
+++ javascript-challenge/UFO-level-1/static/js/app.js
@@ -6,7 +6,7 @@ var button = d3.select("#filter-btn");
 var inputField1 = d3.select("#datetime");
 var inputField2 = d3.select("#city");
 var resetbtn = d3.select("#reset-btn");
-var columns = ["datetime", "city", "state", "country", "shape", "comments"]
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 var populate = (dataInput) => {
     dataInput.forEach(ufo_sightings => {
@@ -14,3 +14,18 @@ var populate = (dataInput) => {
         columns.forEach(column => row.append("td").text(ufo_sightings[column]))
     });
 }
+
+populate(data);
+
+// CREATE FILTER 
+button.on("click", () => {
+    d3.event.preventDefault();
+    var inputDate = inputField1.property("value").trim();
+    varinputCity = inputField2.property("value").toLowerCase().trim();
+    var filterDate = data.filter(data => data.datetime === inputDate);
+    console.log(filterDate);
+    var filterCity = data.filter(data => data.city === inputCity);
+    console.log(filterCity);
+    var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
+    console.log(filterData);
+})
