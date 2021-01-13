cc1ba70908880f46ff19d5a567a487c97a14e232
adessoliers <alexdessoliers@yahoo.com>
2021-01-13 13:23:54 -0800

    updating

--- D3-Challenge/assets/js/app.js
+++ D3-Challenge/assets/js/app.js
@@ -1,1 +1,80 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+var chart = d3.select("#scatter").append("div").classed("chart", true);
+
+// CREATE SVG WRAPPER. APPEND SVG GROUP, SHIFT LEFT  TOP MARINGS
+var svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+//APPEND SVG
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+//RETRIEVE DATA FROM CSV AND EXECUTE
+d3.csv("data.csv").then(function(data, err) {
+    if (err) throw err;
+console.log(healthData)
+    healthData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +healthcare;
+    });
+
+    var xLinearScale = d3.scaleLinear().range([0,width]);
+    var yLinearScale = d3.scaleLinear().range([height,0]);
+
+    //Create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    var xMin;
+    var xMax;
+    var yMin;
+    var yMax;
+
+    xMin = d3.min(healthData, function(data) {
+        return data.healthcare;
+    });
+
+    xMax = d3.max(healthData, function(data) {
+        return data.healthcare;
+    });
+
+    yMin = d3.min(healthData, function(data) {
+        return data.healthcare;
+    });
+
+    yMax = d3.max(healthData, function(data) {
+        return data.proverty;
+    });
+
+    xLinearScale.domain([xMin, xMax]);
+
+    console.log(xMin);
+    console.log(yMax);
+
+    //APPEND X AXIS
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+
+});
